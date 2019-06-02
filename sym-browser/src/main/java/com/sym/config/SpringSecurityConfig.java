5f248c6171738fe0aa28a9e782f8ac5c3b16eeb3
sym <sym542569199@gmail.com>
2019-06-02 11:25:42 +0800

    springSecurity认证

--- sym-browser/src/main/java/com/sym/config/SpringSecurityConfig.java
+++ sym-browser/src/main/java/com/sym/config/SpringSecurityConfig.java
@@ -24,23 +24,36 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin() //表单验证
-            .and()
-            .authorizeRequests() // 要对请求进行认证
-            .anyRequest() //任何请求都要认证
-            .authenticated();
+        /*
+         * HttpSecurity里面的方法可以获取各个组件,如formLogin()获取表单登录组件,
+         * 后面的代码就是基于表单验证组件做配置；当配置完表单登录组件后，想配置其它
+         * 组件，就可以通过and()继续获取HttpSecurity对象，调用它其它方法获取其它组件
+         */
+        http.formLogin()//获取表单登录组件
+                //.loginPage("/signIn.html")//配置表单登录页面
+                .loginPage("/loginHandler")//配置表单登录请求，由它来决定返回一个HTML页面，还是一个JSON
+                .usernameParameter("customerName")//配置表单登录时的用户名
+                .passwordParameter("customerPwd")//配置表单登录时的密码
+                .successForwardUrl("/succeedAfterLogin")//登录成功后的跳转页面
+                .failureForwardUrl("/failedAfterLogin")//登录失败后的跳转页面
+                .loginProcessingUrl("/signIn")//指定校验的url，前端登录把请求提交到此url即可
+                .and()//切换到HttpSecurity组件
+                .csrf().disable()//停止CSRF校验
+                .authorizeRequests()//获取授权组件
+                .antMatchers("/signIn","/loginHandler","/signIn.html").permitAll()//登录页不用校验
+                .anyRequest().authenticated();//其它页面需要校验
     }
 
     /**
      * 接口 PasswordEncoder 是用来对用户密码进行加密和匹配的。此接口只有2个方法：
      * String encode(CharSequence rawPassword) -- 将用户原始密码rawPassword加密后返回
      * boolean matches(CharSequence rawPassword, String encodedPassword)
-     *      -- 将用户原始密码rawPassword(从登陆页面用户自己输入的)和加密后的密码(保存在数据库的用户加密后的密码)进行校验
+     * -- 将用户原始密码rawPassword(从登陆页面用户自己输入的)和加密后的密码(保存在数据库的用户加密后的密码)进行校验
      *
      * @return
      */
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- sym-browser/src/main/java/com/sym/controller/UserController.java
+++ sym-browser/src/main/java/com/sym/controller/UserController.java
@@ -1,9 +1,22 @@
 package com.sym.controller;
 
+import com.sym.entity.ResultInfo;
 import com.sym.entity.UserDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,11 +26,77 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    /*
+     * springSecurity会把请求Request缓存起来,他提供了 RequestCache 接口供保存/获取Request
+     * 默认实现类是 HttpSessionRequestCache，它是把Request保存到session中,
+     */
+    private RequestCache requestCache = new HttpSessionRequestCache();
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+
+    /**
+     * 当springSecurity发现一个请求需要认证时，会把请求转发到这里
+     *
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("loginHandler")
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public ResultInfo signIn(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 首先取出封装了用户请求的 HttpServletRequest 对象
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        if (null != savedRequest) {
+            logger.info("请求的待认证的URL地址为={}", savedRequest.getRedirectUrl());
+            // 获取请求头部信息
+            List<String> accept = savedRequest.getHeaderValues("accept");
+            logger.info("HTTP-header,accept={}", accept);
+            for (String s : accept) {
+                // 带有html字样的说明是浏览器发起的请求
+                if (s.contains("html")) {
+                    //response.sendRedirect("/signIn.html");
+                    redirectStrategy.sendRedirect(request,response,"/signIn.html");
+                    return null;
+                }
+            }
+
+        }
+        // 其它情况返回JSON字符串
+        return ResultInfo.failed("请先登录~！");
+
+    }
+
+    /**
+     * 成功登录会将请求转发到这个方法上
+     *
+     * @return
+     */
+    @RequestMapping("succeedAfterLogin")
+    public String loginSucceed() {
+        return "恭喜你已经成功登录了";
+    }
+
+    /**
+     * 登录失败会将请求转发到这个方法上
+     *
+     * @return
+     */
+    @RequestMapping("failedAfterLogin")
+    public String loginFailed() {
+        return "账户或密码错误";
+    }
+
+    /**
+     * 需要认证的请求
+     *
+     * @return
+     */
     @GetMapping("list")
-    public List<UserDto> list(){
-        return Arrays.asList(
-            new UserDto(1L,"张三",23),
-            new UserDto(2L ,"李四",28)
-        );
+    public List<UserDto> list() {
+        return Arrays.asList(new UserDto(1L, "张三", 23), new UserDto(2L, "李四", 28));
     }
 }
